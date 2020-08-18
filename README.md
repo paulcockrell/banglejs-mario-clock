bf87e7ab3d6d2468439744c391ac53d9bbc72aea
Paul Cockrell <paulcockrell@gmail.com>
2020-08-18 10:59:18 +0100

    Add jumping sprites

--- README.md
+++ README.md
@@ -3,38 +3,18 @@ Let's go back in time with this Gameboy inspired Mario retro clock.
 
 Enjoy watching Mario, or one of the other game characters run through a level while showing you the time and date.
 
-> Buy a BangleJS watch [here](https://banglejs.com)
-
-> Install this Mario Clock from the BangleJS [app store](https://banglejs.com/apps/#clock)
-
-## Characters
-
-![](mario-clock-mario.png)
-![](mario-clock-toad.png)
-![](mario-clock-daisy.png)
-
-## Extra info modes
-
-![](mario-clock-temp.png)
-![](mario-clock-bat.png)
-
-## Night mode
-
-![](mario-clock-night-mode.png)
-
-## Sample animation
-
-![](mario-clock-screen-shot.gif)
+![](mario-clock-screen-shot.png)
 
 ## Features
 
 * Multiple characters - swipe the screen right to change the character between `Mario`, `Toad`, and `Daisy`
-* Night and Day modes - swipe left to toggle mode
+* Night and Day modes - swipe left to enter night mode, with 3 levels of darkness before returning to day mode.
 * Smooth animation
 * Awesome 8-bit style grey-scale graphics
 * Mario jumps to change the time, every minute
 * You can make Mario jump by pressing the bottom button (Button 3) on the watch
-* Toggle the info pannel bettween `Date`, `Battery level`, and `Temperature` by pressing the top button (Button 1).
+* Toggle the info pannel bettween `Date`, `Battery level`, and `Temperature` by pressing the top button (Button 1)
+* If you have [GadgetBridge](https://f-droid.org/packages/nodomain.freeyourgadget.gadgetbridge/) installed on your phone, Mario will let you know when you get a new call or notification. You can clear a message by pressing either Button 1 or Button 3
 
 ## Requests
 
--- marioclock-app.js
+++ marioclock-app.js
@@ -16,6 +16,8 @@ const is12Hour = settings["12hour"] || false;
 
 // Screen dimensions
 let W, H;
+// Screen brightness
+let brightness = 1;
 
 let intervalRef, displayTimeoutRef = null;
 
@@ -58,6 +60,7 @@ const ONE_SECOND = 1000;
 const DATE_MODE = "date";
 const BATT_MODE = "batt";
 const TEMP_MODE = "temp";
+const PHON_MODE = "gbri";
 
 let timer = 0;
 let backgroundArr = [];
@@ -67,6 +70,87 @@ let infoMode = DATE_MODE;
 // Used to stop values flapping when displayed on screen
 let lastBatt = 0;
 let lastTemp = 0;
+
+const phone = {
+  get status() {
+    return NRF.getSecurityStatus().connected ? "Yes" : "No";
+  },
+  message: null,
+  messageTimeout: null,
+  messageScrollX: null,
+  messageType: null,
+};
+
+const SETTINGS_FILE = "marioclock.json";
+
+function readSettings() {
+  return require('Storage').readJSON(SETTINGS_FILE, 1) || {};
+}
+
+function writeSettings(newSettings) {
+  require("Storage").writeJSON(SETTINGS_FILE, newSettings);
+}
+
+function phoneOutbound(msg) {
+  Bluetooth.println(JSON.stringify(msg));
+}
+
+function phoneClearMessage() {
+  if (phone.message === null) return;
+
+  if (phone.messageTimeout) {
+    clearTimeout(phone.messageTimeout);
+    phone.messageTimeout = null;
+  }
+  phone.message = null;
+  phone.messageScrollX = null;
+  phone.messageType = null;
+}
+
+function phoneNewMessage(type, msg) {
+  Bangle.buzz();
+
+  phoneClearMessage();
+  phone.messageTimeout = setTimeout(() => phone.message = null, ONE_SECOND * 30);
+  phone.message = msg;
+  phone.messageType = type;
+
+  // Notify user and active screen
+  if (!Bangle.isLCDOn()) {
+    clearTimers();
+    Bangle.setLCDPower(true);
+  }
+}
+
+function truncStr(str, max) {
+  if (str.length > max) {
+    return str.substr(0, max) + '...';
+  }
+  return str;
+}
+
+function phoneInbound(evt) {
+  switch (evt.t) {
+    case 'notify':
+      const sender = truncStr(evt.sender, 10);
+      const subject = truncStr(evt.subject, 15);
+      phoneNewMessage("notify", `${sender} - '${subject}'`);
+      break;
+    case 'call':
+      if (evt.cmd === "accept") {
+        let nameOrNumber = "Unknown";
+        if (evt.name !== null || evt.name !== "") {
+          nameOrNumber = evt.name;
+        } else if (evt.number !== null || evt.number !== "") {
+          nameOrNumber = evt.number;
+        }
+        phoneNewMessage("call", nameOrNumber);
+      }
+      break;
+    default:
+      return null;
+  }
+}
 
 function genRanNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -92,19 +176,31 @@ function switchCharacter() {
 }
 
 function toggleNightMode() {
-  nightMode = !nightMode;
+  if (!nightMode) {
+    nightMode = true;
+    return;
+  }
+
+  brightness -= 0.30;
+  if (brightness <= 0) {
+    brightness = 1;
+    nightMode = false;
+  }
+  Bangle.setLCDBrightness(brightness);
 }
 
 function incrementTimer() {
-  if (timer > 1000) {
+  if (timer > 100) {
     timer = 0;
   }
   else {
-    timer += 50;
+    timer += 10;
   }
 }
 
 function drawBackground() {
+  "ram"
+
   // Clear screen
   const bgColor = (nightMode) ? NIGHT : LIGHTEST;
   g.setColor(bgColor);
@@ -113,10 +209,10 @@ function drawBackground() {
   // set cloud colors and draw clouds
   const cloudColor = (nightMode) ? DARK : LIGHT;
   g.setColor(cloudColor);
-  g.fillRect(0, 10, g.getWidth(), 15);
-  g.fillRect(0, 17, g.getWidth(), 17);
-  g.fillRect(0, 19, g.getWidth(), 19);
-  g.fillRect(0, 21, g.getWidth(), 21);
+  g.fillRect(0, 10, W, 15);
+  g.fillRect(0, 17, W, 17);
+  g.fillRect(0, 19, W, 19);
+  g.fillRect(0, 21, W, 21);
 
   // Date bar
   g.setColor(DARKEST);
@@ -131,7 +227,9 @@ function drawFloor() {
 }
 
 function drawPyramid() {
-  const pPol = [pyramidSprite.x + 10, H - 6, pyramidSprite.x + 50, pyramidSprite.height, pyramidSprite.x + 90, H - 6]; // Pyramid poly
+  "ram"
+
+  const pPol = [pyramidSprite.x + 10, H - 5, pyramidSprite.x + 50, pyramidSprite.height, pyramidSprite.x + 90, H - 5]; // Pyramid poly
 
   const color = (nightMode) ? DARK : LIGHT;
   g.setColor(color);
@@ -164,7 +262,7 @@ function drawTrees() {
   // remove first sprite if offscreen
   let firstBackgroundSprite = backgroundArr[0];
   if (firstBackgroundSprite) {
-      if (firstBackgroundSprite.x < -15) backgroundArr.splice(0, 1);
+    if (firstBackgroundSprite.x < -15) backgroundArr.splice(0, 1);
   }
 
   // set background sprite if array empty
@@ -210,48 +308,85 @@ function drawCoin() {
 }
 
 function drawDaisyFrame(idx, x, y) {
+  var frame;
+
   switch(idx) {
+    case 2:
+      frame = require("heatshrink").decompress(atob("h0UxH+AAkrAIgAH60rAIQNIBQIABDZErAAwMMBwo0CBxQNEHAQGCBpIPCBoQJCDRIXDBpA7DBIQACw5yCJQgZDP4gNErlcJAZ6GAgNcw+HRI4CCDgNcU44ZDDYSYGDIYACB4QaEDYgMFJAg3DFQ5mFBQYA==")); // daisy jumping
+      break;
     case 0:
-      const dFr1 = require("heatshrink").decompress(atob("h8UxH+AAsHAIgAI60HAIQOJBYIABDpMHAAwNNB4wOJB4gIEHgQBBBxYQCBwYLDDhIaEBxApEw4qDAgIOHDwiIEBwtcFIRWIUgWHw6TIAQXWrlcWZAqBDQIeBBxQaBDxIcCHIQ8JDAIAFWJLPHA=="));
-      g.drawImage(dFr1, x, y);
+      frame = require("heatshrink").decompress(atob("h8UxH+AAsHAIgAI60HAIQOJBYIABDpMHAAwNNB4wOJB4gIEHgQBBBxYQCBwYLDDhIaEBxApEw4qDAgIOHDwiIEBwtcFIRWIUgWHw6TIAQXWrlcWZAqBDQIeBBxQaBDxIcCHIQ8JDAIAFWJLPHA=="));
       break;
     case 1:
     default:
-      const dFr2 = require("heatshrink").decompress(atob("h8UxH+AAsHAIgAI60HAIQOJBYIABDpMHAAwNNB4wOJB4gIEHgQBBBxYQCBwYLDDhIaEBxApEw4qDAgIOHDwiIEBwtcFIRWIUgQvBSZACCBwNcWZQcCAAIPIDgYACFw4YBDYIOCD4waEDYI+HaBQ="));
-      g.drawImage(dFr2, x, y);
+      frame = require("heatshrink").decompress(atob("h8UxH+AAsHAIgAI60HAIQOJBYIABDpMHAAwNNB4wOJB4gIEHgQBBBxYQCBwYLDDhIaEBxApEw4qDAgIOHDwiIEBwtcFIRWIUgQvBSZACCBwNcWZQcCAAIPIDgYACFw4YBDYIOCD4waEDYI+HaBQ="));
   }
+
+  g.drawImage(frame, x, y);
 }
 
 function drawMarioFrame(idx, x, y) {
+  var frame;
+
   switch(idx) {
+    case 2:
+      frame = require("heatshrink").decompress(atob("h8UxH+AAkrAAYFCBo9cAAIEB63WB4gMDB4YOFBowfDw4xDBAYADA4YcDGwYACDoYAEBYYBBw4NDCoYOFDIweFFwoZFAQYIDLAQWGEwqgECI6ECJ4JeGQYS9EB4QTHBwImCBYRtDSAwrFawqkFWY7PEBxoMFKoZaELoYICAAg")); // Mario frame jumping
+      break;
     case 0:
-      const mFr1 = require("heatshrink").decompress(atob("h8UxH+AAkHAAYKFBolcAAIPIBgYPDBpgfGFIY7EA4YcEBIPWAAYdDC4gLDAII5ECoYOFDogODFgoJCBwYZCAQYOFBAhAFFwZKGHQpMDw52FSg2HAAIoDAgIOMB5AAFGQTtKeBLuNcQwOJFwgJFA=")); // Mario Frame 1
-      g.drawImage(mFr1, x, y);
+      frame = require("heatshrink").decompress(atob("h8UxH+AAkrAAYKFBolcAAIPIBgYPDBpgfGFIY7EA4YcEBIPWAAYdDC4gLDAII5ECoYOFDogODFgoJCBwYZCAQYOFBAhAFFwZKGGQgNCw4ACLwgFBBwgKECQpZCCgRqDFQikEJIriIBgzwIdxjiGBxIuEBIo=")); // Mario Frame 1
       break;
     case 1:
     default:
-      const mFr2 = require("heatshrink").decompress(atob("h8UxH+AAkHAAYKFBolcAAIPIBgYPDBpgfGFIY7EA4YcEBIPWAAYdDC4gLDAII5ECoYOFDogODFgoJCBwYZCAQYOFBAhAFFwZKGHQpMDw+HCQYEBSowOBBQIdCCgTOIFgiVHFwYCBUhA9FBwz8HAo73GACQA=")); // Mario frame 2
-      g.drawImage(mFr2, x, y);
+      frame = require("heatshrink").decompress(atob("h8UxH+AAkrAAYKFBolcAAIPIBgYPDBpgfGFIY7EA4YcEBIPWAAYdDC4gLDAII5ECoYOFDogODFgoJCBwYZCAQYOFBAhAFFwZKGHQpMDw+HCQYEBSowOBBQIeJDAQODSwaVHUhwOLfg4FHe4wASA=")); // Mario frame 2
   }
+
+  g.drawImage(frame, x, y);
 }
 
 function drawToadFrame(idx, x, y) {
+  var frame;
+
   switch(idx) {
+    case 2:
+      frame = require("heatshrink").decompress(atob("iEUxH+ACkrAAoNJrnWAAQRGlfWrgACB4QEBCAYOBB44QFB4QICAg4QBBAQbDEgwPCHpAGCGAQ9KAYQPENwoTEH4crw4EDAAgGDB4YABAYIBDP4YLEAAIPHCAQHCCAQTDD4gHDEA4PFGAY3EbooPECob8IPooPFCATGEf44hFAAYLDA==")); // toad jumping
+      break;
     case 0:
-      const tFr1 = require("heatshrink").decompress(atob("iEUxH+ACkHAAoNJrnWAAQRGg/WrgACB4QEBCAYOBB44QFB4QICAg4QBBAQbDEgwPCHpAGCGAQ9KAYQPKCYg/EJAoADAwaKFw4BEP4YQCBIIABB468EB4QADYIoQGDwQOGBYYrCCAwbFFwgQEM4gAEeA4OIH4ghFAAYLD")); // Toad Frame 1
-      g.drawImage(tFr1, x, y);
+      frame = require("heatshrink").decompress(atob("iEUxH+ACkHAAoNJrnWAAQRGg/WrgACB4QEBCAYOBB44QFB4QICAg4QBBAQbDEgwPCHpAGCGAQ9KAYQPKCYg/EJAoADAwaKFw4BEP4YQCBIIABB468EB4QADYIoQGDwQOGBYYrCCAwbFFwgQEM4gAEeA4OIH4ghFAAYLD")); // Toad Frame 1
       break;
     case 1:
     default:
-      const tFr2 = require("heatshrink").decompress(atob("iEUxH+ACkHAAoNJrnWAAQRGg/WrgACB4QEBCAYOBB44QFB4QICAg4QBBAQbDEgwPCHpAGCGAQ9KAYQPKCYg/EJAoADAwaKFw4BEP4YQCBIIABB468EB4QADYIoQGDwQOGBYQrDb4wcGFxYLDMoYgHRYgwKABAMBA")); // Mario frame 2
-      g.drawImage(tFr2, x, y);
+      frame = require("heatshrink").decompress(atob("iEUxH+ACkHAAoNJrnWAAQRGg/WrgACB4QEBCAYOBB44QFB4QICAg4QBBAQbDEgwPCHpAGCGAQ9KAYQPKCYg/EJAoADAwaKFw4BEP4YQCBIIABB468EB4QADYIoQGDwQOGBYQrDb4wcGFxYLDMoYgHRYgwKABAMBA")); // Mario frame 2
   }
+
+  g.drawImage(frame, x, y);
+}
+
+// Mario speach bubble
+function drawNotice(x, y) {
+  if (phone.message === null) return;
+
+  let img;
+  switch (phone.messageType) {
+    case "call":
+      img = require("heatshrink").decompress(atob("h8PxH+AAMHABIND6wAJB4INEw9cAAIPFBxAPEBw/WBxYACDrQ7QLI53OSpApDBoQAHB4INLByANNAwo="));
+      break;
+    case "notify":
+      img = require("heatshrink").decompress(atob("h8PxH+AAMHABIND6wAJB4INCrgAHB4QOEDQgOIAIQFGBwovDA4gOGFooOVLJR3OSpApDBoQAHB4INLByANNAwoA="));
+      break;
+    case "lowBatt":
+      img = require("heatshrink").decompress(atob("h8PxH+AAMHABIND6wAJB4INFrgABB4oOEBoQPFBwwDGB0uHAAIOLJRB3OSpApDBoQAHB4INLByANNAwo"));
+      break;
+  }
+
+  if (img) g.drawImage(img, characterSprite.x, characterSprite.y - 16);
 }
 
 function drawCharacter(date, character) {
+  "ram"
+
   // calculate jumping
   const seconds = date.getSeconds(),
-        milliseconds = date.getMilliseconds();
+    milliseconds = date.getMilliseconds();
 
   if (seconds == 59 && milliseconds > 800 && !characterSprite.isJumping) {
     characterSprite.isJumping = true;
@@ -272,9 +407,13 @@ function drawCharacter(date, character) {
   }
 
   // calculate animation timing
-  if (timer % 50 === 0) {
+  if (timer % 20 === 0) {
     // shift to next frame
-    characterSprite.frameIdx ^= 1;
+    if (characterSprite.isJumping) {
+      characterSprite.frameIdx = 2;
+    } else {
+      characterSprite.frameIdx = characterSprite.frameIdx == 0 ? 1 : 0;
+    }
   }
 
   switch(characterSprite.character) {
@@ -352,31 +491,62 @@ function buildTempStr() {
   return tempStr;
 }
 
+function buildPhonStr() {
+  return `Phone: ${phone.status}`;
+}
+
 function drawInfo(date) {
+  let xPos;
   let str = "";
-  switch(infoMode) {
-    case TEMP_MODE:
-      str = buildTempStr();
-      break;
-    case BATT_MODE:
-      str = buildBatStr();
-      break;
-    case DATE_MODE:
-    default:
-      str = buildDateStr(date);
+
+  if (phone.message !== null) {
+    str = phone.message;
+    const strLen = g.stringWidth(str);
+    if (strLen > W) {
+      if (phone.messageScrollX === null || (phone.messageScrollX <= (strLen * -1))) {
+        phone.messageScrollX = W;
+        resetDisplayTimeout();
+      } else {
+        phone.messageScrollX -= 2;
+      }
+      xPos = phone.messageScrollX;
+    } else {
+      xPos = (W - g.stringWidth(str)) / 2;
+    }
+  } else {
+    switch(infoMode) {
+      case PHON_MODE:
+        str = buildPhonStr();
+        break;
+      case TEMP_MODE:
+        str = buildTempStr();
+        break;
+      case BATT_MODE:
+        str = buildBatStr();
+        break;
+      case DATE_MODE:
+      default:
+        str = buildDateStr(date);
+    }
+    xPos = (W - g.stringWidth(str)) / 2;
   }
 
   g.setFont("6x8");
   g.setColor(LIGHTEST);
-  g.drawString(str, (W - g.stringWidth(str))/2, 1);
+  g.drawString(str, xPos, 1);
 }
 
 function changeInfoMode() {
+  phoneClearMessage();
+
   switch(infoMode) {
     case BATT_MODE:
       infoMode = TEMP_MODE;
       break;
     case TEMP_MODE:
+      infoMode = PHON_MODE;
+      break;
+    case PHON_MODE:
       infoMode = DATE_MODE;
       break;
     case DATE_MODE:
@@ -399,6 +569,7 @@ function redraw() {
   drawTime(date);
   drawInfo(date);
   drawCharacter(date);
+  drawNotice();
   drawCoin();
 
   // Render new frame
@@ -435,8 +606,39 @@ function startTimers(){
   redraw();
 }
 
+function loadSettings() {
+  const settings = readSettings();
+  if (!settings) return;
+
+  if (settings.character) characterSprite.character = settings.character;
+  if (settings.nightMode) nightMode = settings.nightMode;
+  if (settings.brightness) {
+    brightness = settings.brightness;
+    Bangle.setLCDBrightness(brightness);
+  }
+}
+
+function updateSettings() {
+  const newSettings = {
+    character: characterSprite.character,
+    nightMode: nightMode,
+    brightness: brightness,
+  };
+  writeSettings(newSettings);
+}
+
+function checkBatteryLevel() {
+  if (Bangle.isCharging()) return;
+  if (E.getBattery() > 10) return;
+  if (phone.message !== null) return;
+
+  phoneNewMessage("lowBatt", "Warning, battery is low");
+}
+
 // Main
 function init() {
+  loadSettings();
+
   clearInterval();
 
   // Initialise display
@@ -450,6 +652,7 @@ function init() {
   setWatch(() => {
     if (intervalRef && !characterSprite.isJumping) characterSprite.isJumping = true;
     resetDisplayTimeout();
+    phoneClearMessage(); // Clear any phone messages and message timers
   }, BTN3, {repeat: true});
 
   // Close watch and load launcher app
@@ -485,9 +688,30 @@ function init() {
       default:
         toggleNightMode();
     }
+
+    updateSettings();
   });
 
+  // Phone connectivity
+  try { NRF.wake(); } catch (e) {}
+
+  NRF.on('disconnect', () => {
+    phoneNewMessage(null, "Phone disconnected");
+  });
+
+  NRF.on('connect', () => {
+    setTimeout(() => {
+      phoneOutbound({ t: "status", bat: E.getBattery() });
+    }, ONE_SECOND * 2);
+    phoneNewMessage(null, "Phone connected");
+  });
+
+  GB = (evt) => phoneInbound(evt);
+
   startTimers();
+
+  setInterval(checkBatteryLevel, ONE_SECOND * 60 * 10);
+  checkBatteryLevel();
 }
 
 // Initialise!
